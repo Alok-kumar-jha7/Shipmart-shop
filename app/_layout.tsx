813aa7635b7b839c510e831290f683caeded6fdb
Alok-kumar-jha7 <aalok3828@gmail.com>
2026-02-05 22:09:57 +0530

    chore: update dependencies and add Tailwind CSS configuration
    - Added nativewind and prettier-plugin-tailwindcss to package.json
    - Updated react-native-reanimated and react-native-safe-area-context versions
    - Added autoprefixer, postcss, and tailwindcss to devDependencies
    - Created tailwind.config.js for Tailwind CSS setup
    - Updated tsconfig.json to include nativewind-env.d.ts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,10 +3,10 @@ import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
-  <Stack screenOptions={{ headerShown: false }} >
-    <Stack.Screen name="screens/OnBoarding"/>
-    <Stack.Screen name="screens/SignIn"/>
+    <Stack screenOptions={{ headerShown: false }} >
+      <Stack.Screen name="screens/OnBoarding" />
+      <Stack.Screen name="screens/SignIn" />
     </Stack>
     // <StatusBar style='dark'/>
-   )
+  )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,7 @@
 import { Text, View } from "react-native";
 import OnBoarding from "./screens/OnBoarding";
+import '../global.css';
+
 
 export default function Index() {
   return <OnBoarding/>
